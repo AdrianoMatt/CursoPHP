ea6d15edcd3da322dfdb5503d0602024b7359ffb
Adriano Afonso Matt <adriano.matt@gpb.local>
2022-04-20 12:25:41 -0300

    ajustes

---
+++ Condicionais/index.php
@@ -0,0 +1,19 @@
+<?php
+#Declaração de instruções condicionais
+#IF - definição de blocos de código que só executam caso condição seja true
+
+$nome = 'Adriano';
+if($nome =='Adriano')
+{
+echo 'O nome foi validado.';
+}
+
+echo '<br>';
+echo '<br>';
+echo '<br>';
+
+#If e ELSE
+$idade = 24;
+if($idade <=24)
+{echo 'Jovem';}
+else{echo 'Museu';}
--- Strings/index.php
+++ Strings/index.php
@@ -10,23 +10,28 @@ $completo = $nome." ".$sobrenome;
 
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Atividade de String</title>
-</head>
-<body>
-<h3>O meu nome é:</h3>    
-<h1><?= "$nome $sobrenome"?></h1>
-<br>
-<?= "O meu nome é ". strtoupper($nome) ." e o sobrenome é ". strtoupper($sobrenome) . " completo fica: " . strtoupper($completo) ?>
-<br>
+    <!DOCTYPE html>
+    <html lang="en">
 
-<!?= "O meu nome tem ". $caractere ." caracteres"?>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Atividade de String</title>
+    </head>
 
-<!--comentario aleatório-->
-</body>
-</html>
+    <body>
+        <h3>O meu nome é:</h3>
+        <h1>
+            <?= "$nome $sobrenome"?>
+        </h1>
+        <br>
+        <?= "O meu nome é ". strtoupper($nome) ." e o sobrenome é ". strtoupper($sobrenome) . " completo fica: " . strtoupper($completo) ?>
+            <br>
+
+            <!?= "O meu nome tem ". $caractere ." caracteres"?>
+
+            <!--comentario aleatório-->
+    </body>
+
+    </html>
