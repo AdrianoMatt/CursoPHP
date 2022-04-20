b74a2b4539cff3177788299efd5e36a508d16812
Adriano Afonso Matt <adriano.matt@gpb.local>
2022-04-20 17:12:15 -0300

    atualização

--- Condicionais/index.php
+++ Condicionais/index.php
@@ -17,3 +17,22 @@ $idade = 24;
 if($idade <=24)
 {echo 'Jovem';}
 else{echo 'Museu';}
+
+echo '<br>';
+echo '<br>';
+echo '<br>';
+
+#IF, ELSEIF e ELSE
+$nota = 9;
+if($nota <=2){
+    echo 'Nota fraca :(';}
+    elseif($nota <=4){
+        echo 'Nota insulficiente :(';
+    }
+    elseif($nota <=6){
+        echo 'Nota Positiva!';}
+        elseif($nota <=8){
+            echo 'Nota muito positiva!!';}
+            else{
+                echo 'Nota excelente!!!';
+            }
