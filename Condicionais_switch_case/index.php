a46c0f26777cc86c6df61823f64b204e1e046333
Adriano Afonso Matt <adriano.matt@gpb.local>
2022-04-26 10:12:07 -0300

    atualização

--- Condicionais_switch_case/index.php
+++ Condicionais_switch_case/index.php
@@ -2,3 +2,28 @@
 #Declaração/Instruções condicionais
 #SWITCH - alternativa de IF
 
+$nome = 'joao';
+switch ($nome){
+    case 'joao':
+    #codigo 1
+    break;
+    case 'ana':
+    #codigo 2
+    break;
+    default:
+    #codigo 3
+    break;
+}
+
+switch ($nome){
+    case 'joao':
+        echo '1';
+        break;
+    case 'ana':
+        echo '2';
+        break;
+    default:
+    echo 'outra coisa qualquer';
+    break;
+}
+
