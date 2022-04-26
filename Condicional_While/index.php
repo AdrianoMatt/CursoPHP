60a5b6f3fc5b3dd129f956828eeb57d72dcd7d51
Adriano Afonso Matt <adriano.matt@gpb.local>
2022-04-26 17:32:43 -0300

    atualização

---
+++ Condicional_While/index.php
@@ -0,0 +1,14 @@
+<?php
+
+#CICLOS/LOOPS
+#Existem 4 estruturas de ciclos em PHP.
+#Elas existem para permitir executar um determinado bloco de código varias vezes.
+
+#WHILE
+#Definimos a condição durante a qual o ciclo deve continuar a ser executado.
+#Quando essa condição for falsa, o ciclo termina e o código avança
+$x = 1;
+while ($x < 10){
+    echo 'ciclo em execução <br>';
+    $x++;
+}
